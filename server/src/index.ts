926f419f933110fce98867823460f7f3ceedb405
Ngo Gia Phong <ngogiaphong123@gmail.com>
2022-10-13 15:44:02 +0700

    make it better

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000
 const app = express()
 app.use(express.json());
 app.use(cors({
-    origin: process.env.CLIENT_URL,
+    origin: true,
     credentials: true
 }))
 app.use(express.urlencoded({ extended: true }));

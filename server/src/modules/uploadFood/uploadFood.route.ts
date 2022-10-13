848b84edbda1f9a4521ecbc2a09391cf8da19be1
Ngo Gia Phong <ngogiaphong123@gmail.com>
2022-10-13 09:40:15 +0700

    prisma generate

--- server/src/modules/uploadFood/uploadFood.route.ts
+++ server/src/modules/uploadFood/uploadFood.route.ts
@@ -5,10 +5,11 @@ import multer from 'multer'
 import { storage } from '../../../cloudinary'
 import requireAdmin from '../../middlewares/requireAdmin'
 import { processRequestBody, processRequestParams } from 'zod-express-middleware'
+import catchAsync from '../../utils/catchAsync';
 const upload = multer({ storage })
 const uploadFoodRouter = express.Router()
 
-uploadFoodRouter.post('/',upload.single('image'),processRequestBody(uploadFoodSchema),uploadFoodController)
-uploadFoodRouter.get('/',getUploadedFoodController)
-uploadFoodRouter.delete('/:uploadFoodId',processRequestParams(deleteUploadedFoodSchema),deleteUploadedFoodController)
+uploadFoodRouter.post('/',upload.single('image'),processRequestBody(uploadFoodSchema),catchAsync(uploadFoodController))
+uploadFoodRouter.get('/',catchAsync(getUploadedFoodController))
+uploadFoodRouter.delete('/:uploadFoodId',processRequestParams(deleteUploadedFoodSchema),catchAsync(deleteUploadedFoodController))
 export default uploadFoodRouter

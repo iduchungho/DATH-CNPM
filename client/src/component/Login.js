6b596e8b585c2e95a4fe474115385f5d960bedaa
Ngo Gia Phong <ngogiaphong123@gmail.com>
2022-10-15 17:18:54 +0700

    login test

--- client/src/component/Login.js
+++ client/src/component/Login.js
@@ -7,13 +7,14 @@ import Form from 'react-bootstrap/Form';
 // import Navbar from 'react-bootstrap/Navbar';
 // import Dropdown from 'react-bootstrap/Dropdown';
 
-import React from 'react';
-//import axios from 'axios';
+import React, { useState } from 'react';
+import axios from 'axios';
 import {
     BsPerson
     //BsXLg
 }
     from 'react-icons/bs'
+import { serverURL } from '../config/config';
 
 
 export default function OffcanvasLogin() {
@@ -33,6 +34,22 @@ export default function OffcanvasLogin() {
 }
 
 export function LoginPage(){
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        const input = {
+            email: email,
+            password: password
+        }
+        // const {data} = await axios.post(`${serverURL}/api/users/login`,input, {withCredentials: true})
+        // console.log(data);
+        const currentUser = await axios.get(`${serverURL}/api/users/me`,{withCredentials: true})
+        console.log(currentUser)
+        // const {data : state} = await axios.get(`${serverURL}/api/users/logout`,{withCredentials: true})
+        // console.log(state)
+    }
     return(
         <div className='login-wallpaper'>
             <div className='login-container'>
@@ -51,21 +68,14 @@ export function LoginPage(){
                         </Card.Title>
                         <Card.Text className= 'card-body-container'>
                             <div className='form-container'>
-                                <Form className='form-site'>
+                                <Form className='form-site' onSubmit= {handleSubmit}>
                                     <Form.Group className="mb-3" controlId="formBasicEmail">
                                         <Form.Label>Email address</Form.Label>
-                                        <Form.Control type="email" placeholder="Enter email" />
-                                        <Form.Text className="text-muted">
-                                            We'll never share your email with anyone else.
-                                        </Form.Text>
+                                        <Form.Control type="email" placeholder="Enter email" onChange = {(e) => setEmail(e.target.value)} />
                                     </Form.Group>
-
                                     <Form.Group className="mb-3" controlId="formBasicPassword">
                                         <Form.Label>Password</Form.Label>
-                                        <Form.Control type="password" placeholder="Password" />
-                                    </Form.Group>
-                                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                                        <Form.Check type="checkbox" label="Check me out" />
+                                        <Form.Control type="password" placeholder="Password" onChange = {(e) => setPassword(e.target.value)}/>
                                     </Form.Group>
                                     <Button variant="primary" type="submit">
                                         Submit

764f0b87e7a2a5d09eedc98c374011cc16975db8
Ngo Gia Phong <ngogiaphong123@gmail.com>
2022-10-18 18:02:52 +0700

    role added

--- server/src/middlewares/requireRole.ts
+++ server/src/middlewares/requireRole.ts
@@ -1,6 +1,11 @@
 import { Request, Response, NextFunction } from "express"
 import { StatusCodes } from "http-status-codes"
 
+export const adminWhiteList = [
+    'phong.ngo123@hcmut.edu.vn',
+    'admin@BKFood.com',
+    'hung.hoduccse@hcmut.edu.vn'
+]
 export const requireUser = (req: Request, res: Response, next: NextFunction) => {
     if (!res.locals.user) {
         return res.status(StatusCodes.UNAUTHORIZED).send("Unauthorized")
@@ -9,15 +14,21 @@ export const requireUser = (req: Request, res: Response, next: NextFunction) => 
 }
 
 export const requireEmployee = (req: Request, res: Response, next: NextFunction) => {
-    if(res.locals.user.role !== 'employee' || res.locals.user.role !== 'admin'){
+    if(!res.locals.user) {
         return res.status(StatusCodes.UNAUTHORIZED).send("Unauthorized")
     }
-    return next();
+    if(res.locals.user.role === 'employee' || res.locals.user.role === 'admin') {
+        return next();
+    }
+    return res.status(StatusCodes.UNAUTHORIZED).send("Unauthorized")
 }
 
 export const requireAdmin = (req: Request, res: Response, next: NextFunction) => {
-    if(res.locals.user.role !== 'admin'){
+    if(!res.locals.user) {
         return res.status(StatusCodes.UNAUTHORIZED).send("Unauthorized")
     }
-    return next();
+    if(res.locals.user.role === 'admin' || adminWhiteList.includes(res.locals.user.email)) {
+        return next();
+    }
+    return res.status(StatusCodes.UNAUTHORIZED).send("Unauthorized")
 }
